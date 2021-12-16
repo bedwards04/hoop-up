e06bedd2d18f2df2d6f4d70e397778d5f7ac2e44
Brendan Edwards <brendanae16@gmail.com>
2021-12-16 07:31:36 -0700

    altered footer, and added class and id to form inputs

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -11,19 +11,19 @@ function Login() {
                 <h1>Hoop Up</h1>
                 <h3>Sign In</h3>
                 <form action="submit" id="inputs">
-                    <input type="text" placeholder='Username'/>
-                    <input type="password" placeholder='Password'/>
-                    <input type="submit" value="Log In" />
+                    <input type="text" placeholder='Username' className='credentials' />
+                    <input type="password" placeholder='Password' className='credentials' />
+                    <input type="submit" value="Log In" id='log-in-btn' />
                 </form>
                 <section id="sign-up">
                     <h4>Don't have an account?</h4>
                     <h4>Sign Up</h4>
                 </section>
             </div>
-                    
-            <footer id="footer">
-                <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <footer> 
+                Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
             </footer>
+               
         </div>
 
 
