02d2646f7e5cd89b3a0ab918d8faf89f5e7515ad
Brendan Edwards <brendanae16@gmail.com>
2021-12-16 07:38:13 -0700

    added basketball court image

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -4,6 +4,9 @@ import './register.css'
 function Register() {
     return (
         <div>
+            <div>
+                <img src="./basketball-court.jpg" alt="Basketball Court" id='court-image' />
+            </div>
             <form action="submit" id="register">
                 <h2>Register</h2>
                 <input type="text" placeholder='First Name' />
