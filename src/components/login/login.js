c14b5bcdb358caa9297a518b8bfaeeecc96e585f
Brendan Edwards <brendanae16@gmail.com>
2022-01-20 23:43:35 -0700

    Added axios call to authenticate user credentials for login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import {useState} from 'react';
+import {useNavigate} from "react-router-dom";
 import './login.css';
 
-function Login() {
+function Login(props) {
+    let navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e) {
+        //axios request fires (post)
+        e.preventDefault()
+        axios.post('http://localhost:4550/api/login', {username: username, password: password})
+        .then((res) => {
+            console.log(res)
+            props.setUserId(res.data.id)
+            //navigate to different page
+            navigate('/dashboard')
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    }
+
+    function handlePassword(e) {
+        setPassword(e.target.value);
+    }
+
+    function handleUsername(e) {
+        setUsername(e.target.value);
+    }
+
+    function renderRegister() {
+        navigate('/register');
+    }
+
     return (
         <div id="body">
             <div>
@@ -10,14 +43,14 @@ function Login() {
             <div id="log-in-box">
                 <h1>Hoop Up</h1>
                 <h3>Sign In</h3>
-                <form action="submit" id="inputs">
-                    <input type="text" placeholder='Username' className='credentials' />
-                    <input type="password" placeholder='Password' className='credentials' />
+                <form action="submit" id="inputs" onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Username' className='credentials' onChange={handleUsername} required/>
+                    <input type="password" placeholder='Password' className='credentials' onChange={handlePassword} required/>
                     <input type="submit" value="Log In" id='log-in-btn' />
                 </form>
                 <section id="bottom-login">
                     <h4>Don't have an account?</h4>
-                    <h4 id='sign-up'>Sign Up</h4>
+                    <h4 id='sign-up' onClick={renderRegister}>Sign Up</h4>
                 </section>
             </div>
             <footer> 
