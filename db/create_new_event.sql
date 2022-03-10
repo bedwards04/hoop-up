58d4d2a5aa1404bb94e24dcf9727b1b54053b34b
Brendan Edwards <brendanae16@gmail.com>
2022-03-10 10:30:01 -0700

    rearranged order of columns in events table

--- db/create_new_event.sql
+++ db/create_new_event.sql
@@ -1,3 +1,3 @@
-INSERT INTO events (type, description, location, title, date, start_time, end_time, public, user_id)
+INSERT INTO events (user_id, title, date, start_time, end_time, location, type, public, description)
 VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
 returning *;
