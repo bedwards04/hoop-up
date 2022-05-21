43ed18da825fa27a997bf54c6d5ff9ad01e292a4
Brendan Edwards <brendanae16@gmail.com>
2022-05-20 22:07:23 -0600

    rearranged items in update event

--- db/edit_event_by_id.sql
+++ db/edit_event_by_id.sql
@@ -1,4 +1,4 @@
 UPDATE events
-SET type = $1, description = $2, location = $3, title = $4, date = $5, start_time = $6, end_time = $7, public = $8
-WHERE id = $9
+SET title = $1, date = $2, start_time = $3, end_time = $4, location = $5, type = $6, description = $7
+WHERE id = $8
 returning *;
