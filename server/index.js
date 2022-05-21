e33de490d22520e0cf2e7fbfd697f2eeebb995cd
Brendan Edwards <brendanae16@gmail.com>
2022-05-20 22:11:54 -0600

    deleted endpoints that were not longer being used

--- server/index.js
+++ server/index.js
@@ -16,19 +16,10 @@ app.post('/api/login', controller.checkLoginCredentials);
 app.post('/api/register', controller.registerNewUser);
 
 //dashboard
-app.get('/api/events', controller.getYourEvents);
-app.get('/api/public-events', controller.getPublicEvents); 
+app.get('/api/events', controller.getEvents);
 app.post('/api/new-event', controller.createNewEvent);
 app.put('/api/event/:event_id', controller.editEvent);
 app.delete('/api/event/:event_id', controller.deleteEvent);
-
-// event details and invitations
-app.get('/api/event-details/:event_id', controller.getEventDetails);
-app.get('/api/users', controller.getUsers);
-app.post('/api/invitations', controller.addInvitation);
-app.get('/api/invitations', controller.getInvitations);
-app.put('/api/invitations/:invite_id', controller.acceptInvite);
-
 
 massive({
     connectionString: CONNECTION_STRING,
