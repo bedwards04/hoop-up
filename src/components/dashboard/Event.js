01197be0a5410c059514e3fa2b0692165b34c729
Brendan Edwards <brendanae16@gmail.com>
2022-05-20 22:48:47 -0600

    added functionality to delete component

--- src/components/dashboard/Event.js
+++ src/components/dashboard/Event.js
@@ -1,27 +1,39 @@
 import axios from "axios";
 import React from "react";
+import {useNavigate} from "react-router-dom";
 import './event.css';
 
 function Event(props) {
+    const {event} = props
+    let navigate = useNavigate();
 
-    function getPrivateEvents() {
-        axios.get('http://localhost:4550/api/events', {userId: props.userId})
+    function deleteEvent() {
+        axios.delete(`http://localhost:4550/api/event/${event.id}`)
         .then((res) => {
             console.log(res)
+            window.location.reload()
         })
         .catch((err) => {
             console.log(err)
         })
     }
+
+    function renderEditEventForm() {
+        navigate(`/editevent/${event.id}`)
+    }
     
     return(
-        <div>
-            <img src="./delete.png" alt="delete button" id="delete" />
-            <img src="./editing.png" alt="edit button" id="edit" />
-            <h3>Title</h3>
-            <h3>Date</h3>
-            <h3>Time</h3>
-            <h3>Location</h3>
+        <div id="event">
+            <div id="event-buttons">
+                <img src="./delete.png" alt="delete button" id="delete" className="event-icons" onClick={deleteEvent} />
+                <img src="./editing.png" alt="edit button" id="edit" className="event-icons" onClick={renderEditEventForm} />
+            </div>
+            <h3>{event.title}</h3>
+            <h3>{event.date}</h3>
+            <h3>{event.start_time}-{event.end_time}</h3>
+            <h3>{event.location}</h3>
+            <h3>{event.type}</h3>
+            <h3>{event.description}</h3>
             
         </div>
     )
