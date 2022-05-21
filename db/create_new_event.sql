ba13d08947bea25a0291546dfb8a773783e5b09c
Brendan Edwards <brendanae16@gmail.com>
2022-05-20 21:18:21 -0600

    removed public from query

--- db/create_new_event.sql
+++ db/create_new_event.sql
@@ -1,3 +1,3 @@
-INSERT INTO events (user_id, title, date, start_time, end_time, location, type, public, description)
-VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
+INSERT INTO events (user_id, title, date, start_time, end_time, location, type, description)
+VALUES($1, $2, $3, $4, $5, $6, $7, $8)
 returning *;
